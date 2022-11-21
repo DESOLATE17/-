8c3a2c638da80b97ceca3643607a9b0ba80de7d4
Daria Taktashova <taktashovadasha@yandex.ru>
2022-11-21 16:14:22 +0300

    final commit

--- bot.py
+++ bot.py
@@ -14,15 +14,16 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='1я группа' , callback_data = 1))
-    markup.add(telebot.types.InlineKeyboardButton(text='2я группа' , callback_data = 2))
-    markup.add(telebot.types.InlineKeyboardButton(text='3я группа' , callback_data = 3))
-    markup.add(telebot.types.InlineKeyboardButton(text='4я группа' , callback_data = 4))
-    markup.add(telebot.types.InlineKeyboardButton(text='5я группа' , callback_data = 5))
+    markup.add(telebot.types.InlineKeyboardButton(text='Понедельник' , callback_data = 11))
+    markup.add(telebot.types.InlineKeyboardButton(text='Вторник' , callback_data = 12))
+    markup.add(telebot.types.InlineKeyboardButton(text='Среда' , callback_data = 13))
+    markup.add(telebot.types.InlineKeyboardButton(text='Четверг' , callback_data = 14))
+    markup.add(telebot.types.InlineKeyboardButton(text='Пятница' , callback_data = 15))
+    markup.add(telebot.types.InlineKeyboardButton(text='Суббота' , callback_data = 16))
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEGefdjed_O7xn-xHhkPfaFd98KoBW7hQAClBgAAvdbgEjPGPx-6kVXrisE')
-    bot.send_message(message.chat.id, 'Приветики 2 курс ИУ5, выбери свою группу, чтобы глянуть расписание' , reply_markup=markup)
+    bot.send_message(message.chat.id, 'Приветики студент ИУ5-34Б, выбери день недели, чтобы глянуть расписание' , reply_markup=markup)
 
-group = 0
+group = 4
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
@@ -30,17 +31,8 @@ def query_handler(call):
     sh = []
     days = ['11', '12', '13', '14', '15', '16']
     markup2 = types.InlineKeyboardMarkup()
-    if call.data == '1':
-        group = 1
-    elif call.data == '2':
-        group = 2
-    elif call.data == '3':
-        group = 3
-    elif call.data == '4':
-        group = 4
-    elif call.data == '5':
-        group = 5
-    elif call.data in days:
+
+    if call.data in days:
         sh = parse_user_datafile_bs('page.html', group)
         ch_r =[]
         zn_r = []
